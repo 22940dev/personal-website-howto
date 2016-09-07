f9087abdfdcb6588db449e835ed9f87a13bab116
Muhammad Rivki <Muhammad.Rivki@outlook.com>
2016-09-07 15:16:49 +0700

    update homepage

--- src/Components/HomePage/HomeIntro.js
+++ src/Components/HomePage/HomeIntro.js
@@ -5,7 +5,7 @@ const HomeIntro = () => {
   return (
     <div className="homepage-middle">
       <h3> I'm ikki. </h3>
-      <h3>Frontend engineer who loved eating, traveling</h3>
+      <h3>Frontend engineer who loved food, learn something new, daydream</h3>
       <h4>and <span className="highlight">JavaScript Enthusiast</span>. Want to know more <Link to="/about"><span className="highlight highlight-link">About Me?</span></Link></h4>
     </div>
   );
