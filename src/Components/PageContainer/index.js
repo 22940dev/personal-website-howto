0db527869538b7e4dbb2ee822b17da251f56a20d
Muhammad Rivki <Muhammad.Rivki@outlook.com>
2016-09-12 00:37:13 +0700

    add transition effect

--- src/Components/PageContainer/index.js
+++ src/Components/PageContainer/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
 import SideNav from './SideNav';
 import './PageContainer.css';
 
-const PageContainer = (props) => {
+const PageContainer = ({children, location}) => {
   return (
     <div className="page-container">
       <SideNav />
-      <div className="page-container__content">{props.children}</div>
+      <div className="page-container__content">
+        <ReactCSSTransitionGroup
+          transitionName="animate"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={500}
+        >
+          {React.cloneElement(children, {
+            key: location.pathname
+          })}
+        </ReactCSSTransitionGroup>
+      </div>
     </div>
   );
 };
