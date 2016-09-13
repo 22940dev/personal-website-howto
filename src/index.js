b746e679eebf4fa2a93fad749dd1fe96bc7908a2
Muhammad Rivki <Muhammad.Rivki@outlook.com>
2016-09-13 10:17:32 +0700

    save local changes 2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {Router, browserHistory} from 'react-router';
+import {Router, hashHistory} from 'react-router';
 import routes from './Routes';
 import 'normalize.css/normalize.css';
 import './index.css';
 
 render(
-  <Router routes={routes} history={browserHistory} />,
+  <Router routes={routes} history={hashHistory} />,
   document.getElementById('root')
 );
